bb274e7a587b988cfbb11d500f30d491ee5f6538
Christopher Bishop <cbishop@cibgraphics.com>
2024-04-17 18:06:42 -0600

    main content

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,7 +1,24 @@
 $(function() {
     
-  // Put all your jQuery goodness here
+  $("#page-header").typer({
+    typeSpeed: 60,
+    backspaceSpeed: 20,
+    backspaceDelay: 800,
+    repeatDelay: 1000,
+    repeat:true,
+    autoStart:true,
+    startDelay: 100,
+    strings: [
+        "High School Student looking for that first car?",
+        "College student trying to drive something affordable?",
+        "Single parent with dinged credit who needs better, more reliable transportation without getting gouged?",
+        "Working family needing extra cash?",
+        "Retiree on a fixed income that just isn't enough?"
+      ]
+
+  });
+
+  $('.no-js').hide();
+
   
 });
-
-const inchToCm = (inches) => inches * 2.54;
